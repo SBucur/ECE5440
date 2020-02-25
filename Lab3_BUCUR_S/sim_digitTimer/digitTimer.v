c6ef3a0309b4141c75cc8ba192228aa6e95911e3
Stefan Bucur <sbucur@uh.edu>
2020-02-25 11:59:14 -0600

    digitTimer: complete module and pass testbench simulations
    digitTimer_2 will be tested as well to confirm digitTimer modules can be daisy-chained as expected

--- Lab3_BUCUR_S/sim_digitTimer/digitTimer.v
+++ Lab3_BUCUR_S/sim_digitTimer/digitTimer.v
@@ -2,56 +2,77 @@
 // Author: Stefan Bucur 3153
 // Module: digitTimer
 // Description: scalable digit timer for the FPGA game.
-//  timer talk to adjacent digits to determine next value
+//  timer talks to adjacent digits to determine next value
 
 /*  Visual showing how the timer chains together.
-    Larger digit from the left, lower from right.
+    Connect the larger-value digit to the left, lesser to the right.
                      _____
     borrow_up   <---|     |<--- borrow_dn
                     | d_T |
     noborrow_up --->|_____|---> noborrow_dn
-
-    if counter receives signal to count down via borrow_dn (1),
-        if current value is zero,
-            if parent digit is not zero (noborrow_up == 0)
-                send signal borrow_up
 */
+// the timer counts down via a pulse from its borrow_dn wire connected to either a 
+// less significant digit timer or a 1s clock cycle.
+
+// borrow == "i want to borrow"
+// noborrow == "you cannot borrow"
 
 module digitTimer (
+    reconf, count_default,
     borrow_up, borrow_dn,
-    noborrow_up, noborrow_dn
+    noborrow_up, noborrow_dn,
+    count
     );
 
-    input borrow_dn, noborrow_up;
+    input borrow_dn, noborrow_up, reconf;
+    input [3:0] count_default;
     output borrow_up, noborrow_dn;
+    output [3:0] count;
 
-    reg[3:0] count;
+    reg borrow_up, noborrow_dn;
+    reg [3:0] count;
 
-    // count down if receiving a signal from less siginificant digitTimer to count down one
-    always @ (posedge CLK) begin
-        if (RST == 1'b0)
+    always @ (posedge borrow_dn, posedge reconf) begin
+        if (reconf == 1'b1)
         begin
-            count <= 4'b0000;
+            if(count_default > 9)
+            begin
+                count <= 4'b1001;
+                noborrow_dn <= 1'b0;
+            end
+            else begin
+                count <= count_default;
+                if(count_default == 0)
+                begin
+                    noborrow_dn <= 1'b1;
+                end
+                else begin
+                    noborrow_dn <= 1'b0;
+                end
+            end
             borrow_up <= 1'b1;
-            noborrow_dn <= 1'b1;
         end
         else begin
             if (count == 4'b0000)
             begin
-                borrow_up <= 1'b1;
                 if (noborrow_up == 1'b0)
                 begin
                     // back up to 9, signal to slave digit
-                    cout <= 4'b1001;
+                    count <= 4'b1001;
+                    borrow_up <= 1'b1;
+                    noborrow_dn <= 1'b0;
+                end
+                else begin
+                    borrow_up <= 1'b1;
                     noborrow_dn <= 1'b1;
                 end
-                
             end
             else begin
+                // no edge cases here, subtract when asked by child timer (borrow_dn pulse)
                 count <= count - 4'b0001;
-                borrow_up <= 1'b1;
+                borrow_up <= 1'b0;
+                noborrow_dn <= 1'b0;
             end
         end
     end
-
 endmodule
